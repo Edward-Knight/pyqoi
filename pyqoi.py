9918552698f10fb7ac04a68f47383dc6a9671271
Edward Knight <edw@rdknig.ht>
2021-12-21 21:21:10 +0000

    Add flake8 to pre-commit config

--- pyqoi.py
+++ pyqoi.py
@@ -1,3 +1,3 @@
-"""Pure Python implementation of QOI, the Quite OK Image format https://qoiformat.org."""
+"""Pure Python implementation of QOI, the Quite OK Image format."""
 
 __version__ = "0.0.0"
